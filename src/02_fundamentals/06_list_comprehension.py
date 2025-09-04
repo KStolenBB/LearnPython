6b2de4a2746ba3f3a3c47f2e5132842107164dad
Kjetil Stølen <kjetil.stolen@gmail.com>
2025-09-04 07:26:38 +0200

    Added a few more fundamentals (2)

--- src/02_fundamentals/06_list_comprehension.py
+++ src/02_fundamentals/06_list_comprehension.py
@@ -161,3 +161,32 @@ print(long_timers) # {'Rolf': 3, 'Bob': 7, 'Jen': 15, 'Anne': 11}
 long_timers = list(zip(range(15), friends, time_since_seen))
 print(long_timers) # {'Rolf': 3, 'Bob': 7, 'Jen': 15, 'Anne': 11}
 
+
+###
+
+friends = ["Rolf", "Bob", "Jen", "Anne"]
+counter = 0
+
+for friend in friends:
+    print(counter, friend)
+    counter += 1
+
+#
+for counter, friend in enumerate(friends):
+    print(counter, friend)
+
+#    
+friends_list = list(enumerate(friends))
+print(friends_list)
+
+#
+friends_list = list(zip(range(1, 40), friends))
+print(friends_list)
+
+#
+for counter, friend in enumerate(friends, start=1):
+    print(counter, friend)
+
+#
+friends_list = list(enumerate(friends, start=1))
+print(friends_list)
---
+++ src/02_fundamentals/07_lottery.py
@@ -0,0 +1,43 @@
+import random
+
+# This line loads a set of 6 random numbers from the config file
+lottery_numbers = set(random.sample(range(22), 6))
+print(lottery_numbers)
+
+# Here are your players; find out who has the most numbers matching lottery_numbers!
+players = [
+    {"name": "Rolf", "numbers": {1, 3, 5, 7, 9, 11}},
+    {"name": "Charlie", "numbers": {2, 7, 9, 21, 10, 5}},
+    {"name": "Anna", "numbers": {13, 14, 15, 16, 17, 18}},
+    {"name": "Jen", "numbers": {19, 20, 12, 7, 3, 5}},
+]
+
+# Then, print out a line such as "Jen won 1000.".
+# The winnings are calculated with the formula:
+# 100 ** len(numbers_matched)
+highest_num_matches = 0
+current_winner = None
+for player in players:
+    numbers_matched = len(player["numbers"].intersection(lottery_numbers))
+    if numbers_matched > highest_num_matches:
+        highest_num_matches = numbers_matched
+        current_winner = player
+
+if current_winner:
+    winnings = 100 ** highest_num_matches
+    print(f"{current_winner['name']} won {winnings}.")
+
+
+##
+top_player = players[0]  # start by saying "the top matching player is the first one"
+ 
+for player in players:  # Go over each player
+    matched_numbers = len(player["numbers"].intersection(lottery_numbers))  # Calculate how many numbers they matched
+    if matched_numbers > len(top_player["numbers"].intersection(lottery_numbers)):  # If they matched more than the current top player...
+        top_player = player  # Say this player is the new top player
+ 
+# Calculate their winnings using the formula!
+winnings = 100 ** len(top_player["numbers"].intersection(lottery_numbers))
+ 
+print(f"{top_player['name']} won {winnings}.")
+
